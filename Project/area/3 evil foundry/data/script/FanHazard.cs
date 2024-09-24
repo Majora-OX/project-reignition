6489f1913f4ecf4700d616abe5a1ecbc68e829fa
Chris Margroff <sora3087@gmail.com>
2024-09-23 23:23:24 -0400

    update FanHadazrd script to update based on rotations per second

--- Project/area/3 evil foundry/data/script/FanHazard.cs
+++ Project/area/3 evil foundry/data/script/FanHazard.cs
@@ -1,12 +1,13 @@
+using System;
 using Godot;
-using Project.Core;
 
 namespace Project.Gameplay.Hazards;
 
 public partial class FanHazard : Hazard
 {
 	[Export]
-	private float spinSpeed;
+	// private float spinSpeed;
+	public float RotationsPerSecond;
 	[Export]
 	private bool playSFX;
 
@@ -17,19 +18,33 @@ public partial class FanHazard : Hazard
 	[Export]
 	private NodePath sfx;
 	private AudioStreamPlayer3D _sfx;
-
+	private float _rotationSpeed = 0;
+	private Action<double> Animate;
 	public override void _Ready()
 	{
+		if (RotationsPerSecond != 0)
+		{
+			_rotationSpeed = RotationsPerSecond * Mathf.Tau;
+			Animate = Rotate;
+		}
+		else
+		{
+			Animate = Disabled;
+		}
 		_root = GetNode<Node3D>(root);
 		_sfx = GetNode<AudioStreamPlayer3D>(sfx);
 
 		if (playSFX)
 			_sfx.Play();
 	}
-
-	public override void _PhysicsProcess(double _)
+	private void Disabled(double delta) { }
+	private void Rotate(double delta)
 	{
-		_root.Rotation += Vector3.Forward * spinSpeed * PhysicsManager.physicsDelta;
+		_root.Rotation = _root.Rotation + Vector3.Forward * (_rotationSpeed * (float)delta % Mathf.Tau);
+	}
+	public override void _PhysicsProcess(double delta)
+	{
+		Animate(delta);
 		ProcessCollision();
 	}
 }
